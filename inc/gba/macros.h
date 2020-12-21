6702ae66c3753c8a4171af11e44c5ffcd6c42d0e
Lisa Murray <k4mu1kazum4@gmail.com>
2020-12-21 11:48:19 -0600

    Added RGB macro.

--- inc/gba/macros.h
+++ inc/gba/macros.h
@@ -13,6 +13,7 @@
 #define waitForVSync() while(REG_VCOUNT != SCREEN_HEIGHT);
 #define setVideoMode(mode) REG_DISPCNT = (mode)
 #define keyDown(key) (~REG_KEYS & key)
+#define RGB(r, g, b) (unsigned short)(r | (g << 5) | (b << 10))
 
 #endif
 
