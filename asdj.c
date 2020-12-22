821084fb675f91baa093c6c7c78d3d58d63d1a93
Lisa Murray <k4mu1kazum4@gmail.com>
2020-12-22 12:16:32 -0600

    Cleaned up main module.

--- asdj.c
+++ asdj.c
@@ -1,30 +1,32 @@
 //
 // asdj.c
 //
+// Advanced Sound Dj - Main Module
+//
 
 #include "inc/gba/gba.h"
-#include "inc/point2d.h"
-#include <string.h>
+#include "inc/oamctl.h"
 
 void copyBgPalette (unsigned short* pbgPalette);
 void copyBackground (unsigned short* pbgData);
 
-void moveSprite (pOAMEntry pSprite, int x, int y);
-void hideAllSprites (pOAMEntry pSprite);
-void copySpritesToOAM (pOAMEntry pSprite);
-
-OAMEntry g_pSprite[C_SPRITES];
+// Shared module level variables:
+OAMEntry s_pSprite[C_SPRITES]; // Temporary buffer for all sprites.
 
 int main() {
 	
 	// Init. code:
-	hideAllSprites(g_pSprite);
+	hideAllSprites(s_pSprite);
 	setVideoMode(MODE_4 | BG2_ENABLE | OBJ_ENABLE | OBJ_MAP_1D);
 	
 	// Main loop code:
 	while(1) {
+		
+		// Wait for a frame to be drawn.
 		waitForVSync();
-		copySpritesToOAM(g_pSprite);
+		
+		// Copy the temporary sprite buffer to OAM.
+		copySpritesToOAM(s_pSprite);
 	}
 	
 	return 0;
@@ -51,60 +53,4 @@ void copyBackground (unsigned short* pbgData) {
 	
 }
 
-void moveSprite (pOAMEntry pSprite, int x, int y) {
-	
-	// Nullify current position.
-	pSprite->uAttr1 = pSprite->uAttr1 & ATR1_MASK;
-	pSprite->uAttr0 = pSprite->uAttr0 & ATR0_MASK;
-	
-	// Move sprite X position; protecting attribute 1 flags.
-	pSprite->uAttr1 = pSprite->uAttr1 | ((!ATR1_MASK) & x);
-	
-	// Move sprite Y position; protecting attribute 0 flags.
-	pSprite->uAttr0 = pSprite->uAttr0 | ((!ATR0_MASK) & y);
-	
-}
-
-UPoint2D8 getSpritePos (pOAMEntry pSprite) {
-	
-	UPoint2D8 upnt;
-	upnt.x = pSprite->uAttr1 & (!ATR1_MASK);
-	upnt.y = pSprite->uAttr0 & (!ATR0_MASK);
-	return upnt;
-	
-}
-
-void hideAllSprites (pOAMEntry pSprite) {
-	
-	unsigned short iSprite;
-	
-	for (iSprite = 0; iSprite < C_SPRITES; iSprite++) {
-		moveSprite(&pSprite[iSprite], SCREEN_WIDTH, SCREEN_HEIGHT);
-	}
-	
-}
-
-void copySpritesToOAM (pOAMEntry pSprite) {
-	
-	unsigned short iSprite;
-	
-	// Clear OAM if sprite pointer is null.
-	if (!(pSprite)) {
-		/*for (iSprite = 0; iSprite < C_SPRITES*4; iSprite++) {
-			OAM_Memory[iSprite] = 0;
-		}*/
-		memset(OAM_Memory, 0, C_SPRITES*8);
-	} else {
-		
-		// Create temporary pointer to sprite data.
-		unsigned short* pSpriteTemp;
-		pSpriteTemp = (unsigned short*)pSprite;
-		
-		// Copy sprite data to OAM.
-		for (iSprite = 0; iSprite < C_SPRITES*4; iSprite++) {
-			OAM_Memory[iSprite] = pSpriteTemp[iSprite];
-		}
-	}
-	
-}
-
+// EOF
