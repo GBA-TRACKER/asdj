7d9718112dc6bd58e91a183b2c85b608feb50f5f
Lisa Murray <k4mu1kazum4@gmail.com>
2021-01-17 13:21:44 -0600

    Added saves header.

--- asdj.h
+++ asdj.h
@@ -1,8 +1,26 @@
-//
-// asdj.h
-//
-// Advanced Sound Dj - Main Module Header
-//
+/*
+ * asdj.h
+ * 
+ * Advanced Sound Dj - Main Module Header
+ * 
+ * Copyright 2020-2021 ASDj Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ */
 
 #ifndef _ASDJ_H_
 #define _ASDJ_H_
@@ -18,14 +36,11 @@
 #include "inc/obj/oamctl.h"
 #include "inc/obj/palctl.h"
 #include "inc/obj/flash.h"
-
-// Include C headers:
-#include <stdlib.h>
-#include <string.h>
+#include "inc/obj/saves.h"
 
 // Define types and structures:
 
-// Define functions in main module:
+// Declare functions defined in main module:
 void doKeyInput ();
 void waitTime (u32 length);
 
--- inc/obj/saves.h
+++ inc/obj/saves.h
@@ -1,8 +1,27 @@
-//
-// inc/obj/saves.h
-//
-//
-//
+/*
+ * inc/obj/saves.h
+ * 
+ * Advanced Sound Dj - Saves Module Header
+ * 
+ * Copyright 2020-2021 ASDJ Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
 
 #ifndef _SAVES_H_
 #define _SAVES_H_
@@ -40,9 +59,6 @@ typedef struct tagSAVE_HDR
 	u8 cSongs; // Count of saved songs.
 	PSONG_HDR pSong; // Pointer to song headers.
 } SAVE_HDR, *PSAVE_HDR;
-
-u16 loadSongHeader (PSONG_HDR pSong);
-u16 loadSaveHeader (PSAVE_HDR pSave);
 
 #endif /* _SAVES_H_ */
 
