6db4ae13c2f5f0b4945c36b3e153a5adc2bece82
Lisa Murray <k4mu1kazum4@gmail.com>
2021-01-24 16:15:11 -0600

    Added sample header.

--- inc/obj/saves.h
+++ inc/obj/saves.h
@@ -30,13 +30,42 @@
 #define CCH_MAX_DESC 442
 #define C_MAX_SONG 32
 
+extern const u16 g_saves_cchMaxDesc;
+extern const u8 g_saves_cMaxSong;
+
+// Save system header ID.
+// This mostly exists as an internal security/durability feature.
+typedef struct tagSAVE_HDRID
+{
+	char szId[9]; // Header identifier string.
+	u8 uHdrVer; // Header version code.
+} SAVE_HDRID, *PSAVE_HDRID;
+
+#define SAMPF_LOOP 0x0001
+#define SAMPF_MASK 0x0001
+
+typedef struct tagSAMPLE_HDR
+{
+	SAVE_HDRID hdrId; // ID string is "ASDjSamp\0". This is header v0x00.
+	u16 uFlags; // Contains flags about the sample content.
+	u32 iLoopStart; // Sample index of loop start (ignored if !(uFlags & SAMPF_LOOP)).
+	u32 iLoopEnd; // Sample index of loop end (ignored if !(uFlags & SAMPF_LOOP)).
+	u32 cSamples; // Contains total count of samples (aka sample length).
+	pu8 pSamples; // Array of samples.
+} SAMPLE_HDR, *PSAMPLE_HDR;
+
+
+// Pattern header.
 typedef struct tagPATTERN_HDR
 {
-	u8 uPlaceholder; // Temporary placeholder.
+	void* pPlaceholder; // Temporary placeholder.
 } PATTERN_HDR, *PPATTERN_HDR;
 
+
+// Song metadata.
 typedef struct tagSONG_META
 {
+	SAVE_HDRID hdrId; // ID string is "SongMeta\0". This is header v0x00.
 	char szName[32]; // 32-char buffer for song name.
 	char szAuth[32]; // 32-char buffer for author name.
 	u16 uYear; // 16-bit binary year.
@@ -44,17 +73,20 @@ typedef struct tagSONG_META
 	char* pszDesc; // Pointer to buffer for description.
 } SONG_META, *PSONG_META;
 
+
+// Song header.
 typedef struct tagSONG_HDR
 {
-	char strId[8]; // String identifier containing "ASDjSong".
+	SAVE_HDRID hdrId; // ID string is "ASDjSong\0". This is header v0x00.
 	SONG_META meta; // Pointer to metadata structure.
 	u16 cPatterns; // Count of patterns in pPattern.
 	PPATTERN_HDR pPattern; // Array of patterns.
 } SONG_HDR, *PSONG_HDR;
 
+// Save header.
 typedef struct tagSAVE_HDR
 {
-	char strId[8]; // String identifier containing "ASDjSave".
+	SAVE_HDRID hdrId; // ID string is "ASDjSave\0". This is header v 0x00.
 	u8 cSongs; // Count of saved songs.
 	PSONG_HDR pSong; // Pointer to song headers.
 } SAVE_HDR, *PSAVE_HDR;
